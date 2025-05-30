4f736a6c5e3538f0e645ee4b9eab5d8eb9859fec
Joshua <joshua.ghattas@gmail.com>
2025-05-30 06:35:45 -0500

    Added to the navbar

--- ui/navbar.tsx
+++ ui/navbar.tsx
@@ -1,5 +1,28 @@
 "use client";
 
+import Image from "next/image";
+import Link from "next/link";
+import blazevortexLogo from "@/public/blazevortex.png";
+
 export default function Navbar() {
-    return (<><div className="absolute top-0 flex items-center justify-center w-full"><div className="p-2 m-2">Navbar (work in progress)</div></div></>)
+  return (
+    <>
+      <div className="absolute top-0 flex items-center justify-center w-full">
+        <div className="flex items-center justify-center gap-2 p-2 px-4 m-2 border-2 border-white rounded-3xl bg-black/60 backdrop-blur-md">
+          <Link href={"/"} className="flex items-center gap-2">
+            <Image
+              alt="BlazeVortex Logo"
+              width={100}
+              height={100}
+              src={blazevortexLogo}
+              className="rounded-2xl w-[2rem] sm:w-[3rem] md:w-[3rem] lg:w-[3rem] h-auto"
+            />
+            <h3 className="pl-2 text-3xl font-bold border-l-2 border-l-gray-600">
+              BlazeVortex
+            </h3>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
