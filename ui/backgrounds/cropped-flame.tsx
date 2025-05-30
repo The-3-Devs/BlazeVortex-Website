57b26b7cc7183c244c3265e11d99e54fdf85adc7
Joshua <joshua.ghattas@gmail.com>
2025-05-29 21:25:04 -0500

    Touched up the flame background to be more vibrant.

--- ui/backgrounds/cropped-flame.tsx
+++ ui/backgrounds/cropped-flame.tsx
@@ -2,5 +2,5 @@ import Image from "next/image"
 import flameImage from "@/public/blazevortex-bg.png";
 
 export default function CroppedFlameBG() {
-    return (<><div className="absolute bottom-0 left-0 flex justify-end w-full h-full -z-1 opacity-30"><Image src={flameImage} width={2000} height={2000} alt="Flame Background" className="w-full max-h-screen" /></div></>)
+    return (<><div className="absolute bottom-0 left-0 flex justify-end w-full h-full opacity-50 -z-1"><Image src={flameImage} width={4000} height={4000} alt="Flame Background" className="object-cover w-full max-h-screen" /></div></>)
 }
