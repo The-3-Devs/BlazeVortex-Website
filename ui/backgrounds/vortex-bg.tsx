959e5ea040ac74165926b5d87af37aae162250d8
Joshua <joshua.ghattas@gmail.com>
2025-07-10 20:18:24 -0500

    Refreshed background aesthetic

--- ui/backgrounds/vortex-bg.tsx
+++ ui/backgrounds/vortex-bg.tsx
@@ -1,17 +1,19 @@
 import Image from "next/image";
-import flameImage from "@/public/blazevortex-bg-upscaled.png";
+import flameImage from "@/public/blazevortex-bg-upscaled-circle.png";
 
 export default function VortexBG() {
   return (
     <>
-      <div className="fixed bottom-0 left-0 flex justify-end w-screen h-screen opacity-50 -z-1">
-        <Image
-          src={flameImage}
-          width={4000}
-          height={4000}
-          alt="Flame Background"
-          className="object-cover w-full max-h-screen"
-        />
+      <div className="fixed bottom-0 left-0 flex justify-end w-screen h-screen overflow-visible opacity-50 -z-2 pulsing-image">
+        <div className="flex items-center justify-center">
+          <Image
+            src={flameImage}
+            width={4000}
+            height={4000}
+            alt="Flame Background"
+            className="object-cover! rotate-image mask-type-alpha mask-radial-at-center radial-opacity-mask"
+          />
+        </div>
       </div>
     </>
   );
